6affca32501ce67e2330e5056c655e1f736cb8c6
agungcaproex <agung.caproex@gmail.com>
2018-04-04 13:13:19 +0700

    release 2

--- routes/twotter.js
+++ routes/twotter.js
@@ -2,6 +2,6 @@ const routes = require('express').Router()
 const twotterController = require('../controllers/twotter-controller')
 
 routes.post('/search-tweet', twotterController.searchTweets)
-
+routes.get('/home-timeline', twotterController.getHomeTimeline)
 
 module.exports = routes
