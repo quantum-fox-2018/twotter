f73b9b27f8d71a6ac16641222905bca4aa7afb31
haripermadi <harinurpermadi@gmail.com>
2018-04-04 20:25:12 +0700

    server

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config()
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const tweet = require('./routes/tweet')
 
 var app = express();
-
+app.use(cors())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
