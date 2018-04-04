c0bc9d56510cdb81732d1b55b4807f86a27e2b3b
ekifakhrureza <ekifakhrureza@gmail.com>
2018-04-04 22:52:09 +0700

    fix done

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
-const api = require('./routes/api')
+const cors = require('cors')
+app.options('*', cors()) // include before other routes 
+app.use(cors())
 require('dotenv').config()
+const api = require('./routes/api')
+
+
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
